ca099a63da7caa4e75626fcb66352bc52cadbd96
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2020-03-29 00:25:31 +0100

    Add schema test case

---
+++ test/support/schema_case.ex
@@ -0,0 +1,19 @@
+defmodule Covid19OrientationWeb.SchemaCase do
+  @moduledoc """
+  This module defines the test case to be used by
+  tests that require validating schemas.
+  """
+
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      import Macro, only: [escape: 2]
+      import OpenApiSpex.TestAssertions
+    end
+  end
+
+  setup _tags do
+    :ok
+  end
+end
