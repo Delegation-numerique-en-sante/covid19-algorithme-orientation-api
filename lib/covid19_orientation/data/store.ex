fe78592e6f983f2a4fcca2127464cbc7e299c74f
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2020-03-29 18:28:20 +0200

    Testing persisted ETS

--- lib/covid19_orientation/data/store.ex
+++ lib/covid19_orientation/data/store.ex
@@ -5,18 +5,11 @@ defmodule Covid19Orientation.Data.Store do
 
   use GenServer
 
-  @ets_table_name :orientations
-  @log_limit 1_000_000_000
+  @file_name "data.tab"
+  @table_name :orientations
 
   def start_link(opts \\ []) do
-    GenServer.start_link(
-      __MODULE__,
-      [
-        {:ets_table_name, @ets_table_name},
-        {:log_limit, @log_limit}
-      ],
-      opts
-    )
+    GenServer.start_link(__MODULE__, [{:file_name, @file_name}, {:table_name, @table_name}], opts)
   end
 
   def get(id, timestamp) do
@@ -34,20 +27,20 @@ defmodule Covid19Orientation.Data.Store do
   # GenServer callbacks
 
   def handle_call({:get, key}, _from, state) do
-    %{ets_table_name: ets_table_name} = state
-    result = :ets.lookup(ets_table_name, key)
+    %{table_name: table_name} = state
+    result = :ets.lookup(table_name, key)
     {:reply, result, state}
   end
 
   def handle_call({:set, key, value}, _from, state) do
-    %{ets_table_name: ets_table_name} = state
-    true = :ets.insert(ets_table_name, {key, value})
+    %{table_name: table_name} = state
+    true = :ets.insert(table_name, {key, value})
     {:reply, value, state}
   end
 
   def init(args) do
-    [{:ets_table_name, ets_table_name}, {:log_limit, log_limit}] = args
-    :ets.new(ets_table_name, [:named_table, :set, :public])
-    {:ok, %{log_limit: log_limit, ets_table_name: ets_table_name}}
+    [{:file_name, file_name}, {:table_name, table_name}] = args
+    PersistentEts.new(table_name, file_name, [:named_table, :set, :public])
+    {:ok, %{file_name: file_name, table_name: table_name}}
   end
 end
