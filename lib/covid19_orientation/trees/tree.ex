0ab29d4e9c6d1ffb0ff02d933ed1241901d95c1a
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2020-03-29 11:24:36 +0200

    Reorganise trees

--- lib/covid19_orientation/trees/tree.ex
+++ lib/covid19_orientation/trees/tree.ex
@@ -1,16 +1,19 @@
-defmodule Covid19Orientation.Tree do
+defmodule Covid19Orientation.Trees.Tree do
   @moduledoc """
   Implémentation basique d'un arbre.
   """
 
   alias __MODULE__
-  alias Covid19OrientationWeb.Schemas.Orientation
+  alias Covid19Orientation.Trees.{FlattenTree, TraverseTree}
 
   defstruct key: nil, type: nil, depth: nil, operation: nil, children: []
 
   @type key() :: any
   @type type() :: :branch | :leaf
   @type depth() :: integer
-  @type operation() :: (Orientation.t() -> boolean)
+  @type operation() :: (any -> boolean)
   @type t :: %Tree{key: nil, type: type, depth: depth, operation: operation, children: [t]}
+
+  defdelegate flatten(trees), to: FlattenTree, as: :call
+  defdelegate traverse(stack, value), to: TraverseTree, as: :call
 end
--- test/covid19_orientation/trees/traverse_tree_test.exs
+++ test/covid19_orientation/trees/traverse_tree_test.exs
@@ -1,6 +1,6 @@
-defmodule Covid19Orientation.TreeTraversalTest do
+defmodule Covid19Orientation.Trees.TraverseTreeTest do
   use ExUnit.Case, async: true
-  alias Covid19Orientation.{Tree, TreeTraversal}
+  alias Covid19Orientation.Trees.{FlattenTree, TraverseTree, Tree}
 
   setup do
     operation = &is_integer/1
@@ -29,21 +29,12 @@ defmodule Covid19Orientation.TreeTraversalTest do
      ]}
   end
 
-  test "aplatit l'arbre", %{trees: trees} do
-    types =
-      trees
-      |> TreeTraversal.flatten()
-      |> Enum.map(& &1.type)
-
-    assert types == [:branch, :branch, :leaf, :branch, :branch, :leaf, :leaf]
-  end
-
   describe "traverse l'arbre" do
     test "s'arrête à la première feuille vraie", %{trees: trees} do
       {:ok, tree} =
         trees
-        |> TreeTraversal.flatten()
-        |> TreeTraversal.traverse(1)
+        |> FlattenTree.call()
+        |> TraverseTree.call(1)
 
       assert tree.type == :leaf
     end
@@ -51,8 +42,8 @@ defmodule Covid19Orientation.TreeTraversalTest do
     test "s'arrête à la fin du chemin, s'il n'y a pas de feuille vraie", %{trees: trees} do
       {:ok, ok} =
         trees
-        |> TreeTraversal.flatten()
-        |> TreeTraversal.traverse(1.0)
+        |> FlattenTree.call()
+        |> TraverseTree.call(1.0)
 
       assert ok == :done
     end
