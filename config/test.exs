b898c9ed06e727460ff81dda7992e5cddd9b3682
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2020-03-31 18:46:14 +0200

    Add test config

--- config/test.exs
+++ config/test.exs
@@ -6,5 +6,22 @@ config :covid19_orientation, Covid19OrientationWeb.Endpoint,
   http: [port: 4002],
   server: false
 
+# Configure Redis
+config :covid19_orientation, Covid19Orientation.Data.Store,
+  conn_opts: [
+    host: "127.0.0.1",
+    port: 6379
+  ]
+
+# Configure PostGreSQL
+config :covid19_orientation, Covid19Orientation.Data.PgStore,
+  conn_opts: [
+    hostname: "127.0.0.1",
+    port: 5432,
+    username: "test",
+    password: "test",
+    database: "test"
+  ]
+
 # Print only warnings and errors during test
 config :logger, level: :warn
