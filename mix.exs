ec8daf6d1dca2deab89b2f9a5b8975eead7f6ce1
pmontens <pieterjan@montens.net>
2020-03-31 13:21:31 +0200

    :construction: testing postgresql writer to log requests

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule Covid19Orientation.MixProject do
       {:phoenix, "~> 1.4.16"},
       {:phoenix_pubsub, "~> 1.1"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.3.1", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.3.1", only: [:dev, :test], runtime: false},
+      {:postgrex, "~> 0.15.3"},
     ]
   end
 end
