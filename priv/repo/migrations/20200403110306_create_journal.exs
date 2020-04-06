e20ae3af92c34f952b1bf99d95b8863a37f080ad
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2020-04-06 22:23:10 +0200

    Do not create too many indexes

--- priv/repo/migrations/20200403110306_create_journal.exs
+++ priv/repo/migrations/20200403110306_create_journal.exs
@@ -7,14 +7,9 @@ defmodule Covid19Questionnaire.Data.Repo.Migrations.CreateJournal do
       add(:date, :utc_datetime_usec, null: false)
       add(:data, :jsonb, null: false)
     end
-
-    create index(:journal, [:date], using: :brin)
-    create index(:journal, [:data], using: :gin)
   end
 
   def down do
-    drop index(:journal, [:data])
-    drop index(:journal, [:date])
     drop table(:journal)
   end
 end
