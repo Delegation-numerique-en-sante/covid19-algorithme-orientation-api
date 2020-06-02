4a9f42d52262990f8287857de82137018af891d7
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2020-06-02 10:55:50 +0200

    Test export start date edge cases

--- lib/covid19_questionnaire/data/exporter.ex
+++ lib/covid19_questionnaire/data/exporter.ex
@@ -1,78 +1,110 @@
 defmodule Covid19Questionnaire.Data.Exporter do
-    alias Covid19Questionnaire.Data.{Export, Journal, Repo}
-    import Ecto.Query
+  @moduledoc false
+  alias Covid19Questionnaire.Data.{Export, Journal, Repo}
+  import Ecto.Query
 
-    @chunk_size 500
+  @chunk_size 500
+  @config Application.get_env(:covid19_questionnaire, __MODULE__)
 
-    def export(nil), do: :pass
-    def export(false), do: :pass
-    def export("false"), do: :pass
-    def export(0), do: :pass
+  def export(date = %Date{}) do
+    str_date = Date.to_iso8601(date)
+    conn = connection()
 
-    def export(%Date{} = date) do
-        str_date = Date.to_iso8601(date)
-        conn = sftp_connection()
+    source_stream =
+      Stream.resource(
+        fn -> 0 end,
+        fn
+          :stop ->
+            {:halt, :stop}
 
-        source_stream =
-            Stream.resource(
-                fn -> 0 end,
-                fn
-                    :stop -> {:halt, :stop}
-                    offset ->
-                        rows = Repo.all(from(query(date), limit: ^@chunk_size, offset: ^offset))
-                        if Enum.count(rows) < @chunk_size do
-                            {rows, :stop}
-                        else
-                            {rows, offset + @chunk_size}
-                        end
-                    end,
-                fn _ -> :ok end
-            )
-        
-        target_filename = root_export <> str_date <> ".export.json"    
-        part_filename = target_filename <> ".part"
-        target_stream = SFTPClient.stream_file!(
-            conn,
-            part_filename
-        )
+          offset ->
+            rows = Repo.all(from(query(date), limit: ^@chunk_size, offset: ^offset))
 
-        source_stream
-        |> Stream.map(& Jason.encode!(%{date: &1.date, data: &1.data}) <> "\n")
-        |> Stream.into(target_stream)
-        |> Stream.run()
-
-        SFTPClient.rename(conn, part_filename, target_filename)
-
-        SFTPClient.disconnect(conn)
-        Repo.insert(%Export{date: date})
-    end
-
-    def export(_) do
-        min_date =
-            case Repo.aggregate(Export, :min, :date) do
-                nil ->
-                    query = from j in Journal, select: min(fragment("date(?)", j.date))
-                    Repo.one(query)
-                min -> min
+            if Enum.count(rows) < @chunk_size do
+              {rows, :stop}
+            else
+              {rows, offset + @chunk_size}
             end
-            |> Date.add(1)
+        end,
+        fn _ -> :ok end
+      )
 
-        max_date = Date.utc_today() |> Date.add(-1)
+    target_filename = root_export() <> str_date <> ".export.json"
+    part_filename = target_filename <> ".part"
 
-        date_range = Date.range(min_date, max_date)
-        Enum.each(date_range, &export/1)
+    target_stream =
+      SFTPClient.stream_file!(
+        conn,
+        part_filename
+      )
+
+    source_stream
+    |> Stream.map(&(Jason.encode!(%{date: &1.date, data: &1.data}) <> "\n"))
+    |> Stream.into(target_stream)
+    |> Stream.run()
+
+    SFTPClient.rename(conn, part_filename, target_filename)
+
+    SFTPClient.disconnect(conn)
+    Repo.insert(%Export{date: date})
+  end
+
+  def export do
+    start_date()
+    |> Date.range(yesterday())
+    |> Enum.each(&export/1)
+  end
+
+  def query(date) do
+    from(j in Journal,
+      where: fragment("date(?)", j.date) == ^date
+    )
+  end
+
+  # Helpers
+
+  @doc """
+  Next day of the last export.
+  """
+  @spec start_date :: nil | Date.t()
+  def start_date do
+    case last_export() do
+      nil -> nil
+      exp -> exp |> Date.add(1)
     end
+  end
 
-    defp sftp_connection do
-        SFTPClient.connect!(ftp_config)
+  @doc """
+  Yesterday.
+  """
+  @spec yesterday :: Date.t()
+  def yesterday do
+    Date.utc_today() |> Date.add(-1)
+  end
+
+  # Private
+
+  @spec last_export :: nil | Date.t()
+  defp last_export do
+    case Repo.aggregate(Export, :max, :date) do
+      nil ->
+        query = from(journal in Journal, select: min(fragment("date(?)", journal.date)))
+        Repo.one(query)
+
+      min ->
+        min
     end
+  end
 
-    defp ftp_config, do: Application.get_env(:covid19_questionnaire, __MODULE__)[:sftp]
+  defp connection do
+    SFTPClient.connect!(sftp())
+  end
 
-    def query(date) do
-        from j in Journal,
-            where: fragment("date(?)", j.date) == ^date
-    end
+  defp sftp do
+    @config[:sftp]
+  end
 
-    defp root_export, do: Application.get_env(:covid19_questionnaire, __MODULE__)[:root_export]
+  defp root_export do
+    @config[:root_export]
+  end
 end
